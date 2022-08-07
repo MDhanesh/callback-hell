fa5d61f191762177f970a428fd58db2204a450b8
Dhanesh M <75882748+MDhanesh@users.noreply.github.com>
2022-08-07 14:19:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,48 +2,48 @@ var ele = document.createElement("div");
 var span = document.createElement("span");
 ele.setAttribute("class", "container");
 span.setAttribute("id", "span");
+span.innerHTML = "10";
 ele.append(span);
 document.body.append(ele);
-let spa = document.getElementById("span");
-spa.innerHTML = "10";
+
 let n = 10;
 setTimeout(() => {
   n--;
-  spa.innerHTML = n;
+  span.innerHTML = n;
   setTimeout(() => {
     n--;
-    spa.innerHTML = n;
+    span.innerHTML = n;
     setTimeout(() => {
       n--;
-      spa.innerHTML = n;
+      span.innerHTML = n;
 
       setTimeout(() => {
         n--;
-        spa.innerHTML = n;
+        span.innerHTML = n;
 
         setTimeout(() => {
           n--;
-          spa.innerHTML = n;
+          span.innerHTML = n;
 
           setTimeout(() => {
             n--;
-            spa.innerHTML = n;
+            span.innerHTML = n;
 
             setTimeout(() => {
               n--;
-              spa.innerHTML = n;
+              span.innerHTML = n;
 
               setTimeout(() => {
                 n--;
-                spa.innerHTML = n;
+                span.innerHTML = n;
 
                 setTimeout(() => {
                   n--;
-                  spa.innerHTML = n;
+                  span.innerHTML = n;
 
                   setTimeout(() => {
                     n--;
-                    spa.innerHTML = "HAPPY  INDEPENDENCE  DAY";
+                    span.innerHTML = "HAPPY  INDEPENDENCE  DAY";
                   }, 1000);
                 }, 1000);
               }, 1000);
